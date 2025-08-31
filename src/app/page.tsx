234307eb4c6f1335a8d1454bd245ec45d1cba3b9
Nipuna Amarasooriya <sudarakaeng@gmail.com>
2025-08-31 21:50:57 +0530

    create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,30 @@ import Iridescence from "@/components/Iridescence";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Card from "@/components/Card";
+import { Palette, UsersRound, Folder, HeartHandshake } from "lucide-react";
+
+const cards = [
+  {
+    title: "Interactive Courses",
+    description: "Real-time feedback and hands-on challenges",
+    icon: Palette,
+  },
+  {
+    title: "Expert Mentorship",
+    description: "Learn from industry professionals",
+    icon: UsersRound,
+  },
+  {
+    title: "Portfolio Builder",
+    description: "Showcase your skills professionally",
+    icon: Folder,
+  },
+  {
+    title: "Design Community",
+    description: "Connect and grow with peers",
+    icon: HeartHandshake,
+  },
+];
 
 export default function Home() {
   return (
@@ -45,8 +69,8 @@ export default function Home() {
       </div>
 
       {/* Section 2 without background */}
-      <div className=" z-10 mt-8 h-[1000px] lg:h-[800px] flex items-center justify-center">
-        <div className="flex flex-col">
+      <div className=" z-10 mt-8 h-auto flex flex-col items-center justify-center py-28">
+        <div className="flex flex-col ">
           <h1 className="text-3xl md:text-5xl font-bold text-white text-center">
             What&apos;s Coming
           </h1>
@@ -55,14 +79,52 @@ export default function Home() {
           </p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-32 max-w-7xl">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {cards.map((card) => (
+              <Card
+                key={card.title}
+                title={card.title}
+                description={card.description}
+                icon={card.icon}
+              />
+            ))}
           </div>
         </div>
-        <div></div>
+        <div className=" z-10 mt-8 h-screen flex flex-col w-full max-w-3xl items-center justify-center py-28">
+          <div className="flex flex-col px-10">
+            <h1 className="text-3xl md:text-5xl font-bold text-white text-center">
+              Ready to Transform Your Design Career?
+            </h1>
+            <p className="text-xs md:text-lg mt-2 text-white text-center">
+              Join the waitlist for the next generation of design education
+            </p>
+          </div>
+          <div className="flex flex-col lg:flex-row  w-full mt-20 gap-5 lg:gap-2 px-10">
+            <Input type="email" placeholder="Email" className="py-5" />
+            <Button
+              type="submit"
+              // variant="outline"
+              className="py-5 bg-orange-500 text-white hover:bg-orange-600"
+            >
+              Subscribe
+            </Button>
+          </div>
+        </div>
+
+        {/* footer */}
+        <footer className=" py-10 w-full">
+          <div className="container mx-auto text-center border-t border-gray-800 w-full ">
+            <p className="text-gray-400 mt-12">
+              &copy; {new Date().getFullYear()} PixelSL. All rights reserved.
+            </p>
+          </div>
+        </footer>
       </div>
     </div>
   );
 }
+//  <h1 className="text-3xl md:text-5xl font-bold text-white text-center">
+//               Ready to Transform Your Design Career
+//             </h1>
+//             <p className="text-xs md:text-lg mt-2 text-white text-center">
+//               Join the waitlist for the next generation of design education
+//             </p>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { Palette } from "lucide-react";
 
-export default function Card() {
+type CardProps = {
+  title: string;
+  description: string;
+  icon: React.ElementType; // Accepts a component like Palette
+};
+
+export default function Card({ title, description, icon: Icon }: CardProps) {
   return (
-    <div className="flex flex-col justify-center items-center p-8 ">
+    <div className="flex flex-col justify-center items-center p-8">
       <div className="flex w-[48px] h-[48px] bg-orange-950 rounded-full items-center justify-center">
-        <Palette className="text-orange-500 " />
+        <Icon className="text-orange-500" />
       </div>
-      <h3 className="text-xl font-bold text-center text-white mt-6">
-        Iteractive Courses
-      </h3>
-
+      <h3 className="text-xl font-bold text-center text-white mt-6">{title}</h3>
       <p className="text-center text-gray-500 text-xs px-16 max-w-sm w-full mt-3">
-        Learn the fundamentals of design and get hands-on experience with
-        real-world projects.
+        {description}
       </p>
     </div>
   );
